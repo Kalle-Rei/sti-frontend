59eafedbe44c9f9d2806ba5bc999ec0f7d44ad51
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-02-14 13:00:48 +0100

    handle creation of table in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -20,8 +20,29 @@
 // }
 
 var tableTest = document.getElementById("myTable")
-tableTest.innerHTML = "<button onclick='insertTableRow()'>tableTest</button>"
+var tableTest2 = document.getElementById("myTable")
+tableTest.innerHTML = "<button onclick='insertTableRow()'>Add row</button>"
+tableTest2.innerHTML = "<button onclick='deleteTableRow()'>Delete row</button>"
 
 function insertTableRow(){
     tableTest.insertRow(0)
+}
+
+function deleteTableRow(){
+    tableTest2.deleteRow(0)
+}
+
+function makeTable(){
+    var x = document.createElement("table")
+    x.setAttribute("id", "myTable")
+    document.body.appendChild(x)
+
+    var y = document.createElement("tr")
+    y.setAttribute("id", "myTr")
+    document.getElementById("myTable").appendChild(y)
+
+    var z = document.createElement("td")
+    var t = document.createElement("cell")
+    z.appendChild(t)
+    document.getElementById("myTr").appendChild(z)
 }
