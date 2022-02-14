92204ff5db1ca97c70a90e93dda43746d23d4b65
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-02-14 09:54:31 +0100

    dynamic HTML test

--- public/js/index.js
+++ public/js/index.js
@@ -5,3 +5,7 @@ function age(){
     console.log("Aging game")
     setTimeout(age, 3000)
 }
+
+var anElement = document.getElementById("app")
+anElement.innerHTML = "Hello World"
+anElement.outerHTML = "Hello World"
