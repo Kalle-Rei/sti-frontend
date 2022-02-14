f8df7b52dd7940ef3f1e282609e418581e5f2776
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-02-14 10:45:01 +0100

    still testing stuff

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,20 @@
-console.log("Initializing game")
-setTimeout(age, 3000)
+// console.log("Initializing game")
+// setTimeout(age, 3000)
 
-function age(){
-    console.log("Aging game")
-    setTimeout(age, 3000)
-}
+// function age(){
+//     console.log("Aging game")
+//     setTimeout(age, 3000)
+// }
 
 var anElement = document.createElement("strong")
 anElement.innerHTML = "Hello World"
 
 var app = document.getElementById("app")
 app.appendChild(anElement)
+
+var ettElement = document.getElementById("app")
+ettElement.innerHTML = "<button onclick='buttonClick()'>Click me</button>"
+
+function buttonClick(){
+    ettElement.outerHTML = "<strong>Kalle</strong>"
+}
