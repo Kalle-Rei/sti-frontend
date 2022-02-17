f557e1da0e234db8c73f4c0e9d7fed7980a4b2a5
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-02-17 13:55:57 +0100

    try loading the backend from heroku

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,8 @@ age();
 function age() {
   let xhr = new XMLHttpRequest();
   //xhr.open("GET", "/js/data.json");
-  xhr.open("GET", "http://localhost:3001/football");
+  //xhr.open("GET", "http://localhost:3001/football");
+  xhr.open("GET", "http://kalle-backend.herokuapp.com/");
   xhr.onload = function () {
     let data = JSON.parse(this.response);
     createTable(data);
