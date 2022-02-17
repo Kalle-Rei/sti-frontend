de0ce919520fcda89aa3859beb1bcdee7327d31a
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-02-17 13:52:03 +0100

    removed unused code, fixed formatting

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 //app.js
-const express = require("express")
-let favicon = require('serve-favicon')
-let path = require('path')
-const res = require("express/lib/response")
+const express = require("express");
+let favicon = require("serve-favicon");
+let path = require("path");
+const res = require("express/lib/response");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-const app = express()
-app.use('/healthcheck', require('./routes/healthcheck.routes'));
+const app = express();
+app.use("/healthcheck", require("./routes/healthcheck.routes"));
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + "/public/index.html")
-})
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
-app.get('/football', function(req, res){
-    res.sendFile(__dirname + "/public/index.html")
-})
+app.get("/football", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
-app.listen(PORT, function(){
-    console.log(`Server started on port ${PORT}`)
-})
+app.listen(PORT, function () {
+  console.log(`Server started on port ${PORT}`);
+});
--- public/js/index.js
+++ public/js/index.js
@@ -1,47 +1,47 @@
 //index.js
 
-age(); 
+age();
 
-function age(){
-    let xhr = new XMLHttpRequest();
-    //xhr.open("GET", "/js/data.json");
-    xhr.open("GET", "http://localhost:3001/football")
-    xhr.onload = function(){
-        let data = JSON.parse(this.response);
-        createTable(data);
-    }
-    xhr.send();
+function age() {
+  let xhr = new XMLHttpRequest();
+  //xhr.open("GET", "/js/data.json");
+  xhr.open("GET", "http://localhost:3001/football");
+  xhr.onload = function () {
+    let data = JSON.parse(this.response);
+    createTable(data);
+  };
+  xhr.send();
 }
 
-function createTable(data){
-    let appElement = document.getElementById("app");
-    let aTable = document.createElement("table");
-    appElement.appendChild(aTable);
-    aTable.appendChild(createRow(data[0].name, data[0].points, data[0].logo));
-    aTable.appendChild(createRow(data[1].name, data[1].points, data[1].logo));
-    aTable.appendChild(createRow(data[2].name, data[2].points, data[2].logo));
-    aTable.appendChild(createRow(data[3].name, data[3].points, data[3].logo));
+function createTable(data) {
+  let appElement = document.getElementById("app");
+  let aTable = document.createElement("table");
+  appElement.appendChild(aTable);
+  aTable.appendChild(createRow(data[0].name, data[0].points, data[0].logo));
+  aTable.appendChild(createRow(data[1].name, data[1].points, data[1].logo));
+  aTable.appendChild(createRow(data[2].name, data[2].points, data[2].logo));
+  aTable.appendChild(createRow(data[3].name, data[3].points, data[3].logo));
 }
 
-function createRow(name, points, url){
-    let aRow = document.createElement("tr");
-    aRow.appendChild(createImageCell(url));
-    aRow.appendChild(createCell(name));
-    aRow.appendChild(createCell(points));
-    return aRow;
+function createRow(name, points, url) {
+  let aRow = document.createElement("tr");
+  aRow.appendChild(createImageCell(url));
+  aRow.appendChild(createCell(name));
+  aRow.appendChild(createCell(points));
+  return aRow;
 }
 
-function createCell(content){
-    let aCell = document.createElement("td");
-    aCell.innerHTML = content;
-    return aCell;
+function createCell(content) {
+  let aCell = document.createElement("td");
+  aCell.innerHTML = content;
+  return aCell;
 }
 
-function createImageCell(url){
-    let aCell = document.createElement("td");
-    let anImage = document.createElement("img");
-    anImage.src=url;
-    anImage.classList.add("tableLogo");
-    aCell.appendChild(anImage);
-    return aCell;
+function createImageCell(url) {
+  let aCell = document.createElement("td");
+  let anImage = document.createElement("img");
+  anImage.src = url;
+  anImage.classList.add("tableLogo");
+  aCell.appendChild(anImage);
+  return aCell;
 }
