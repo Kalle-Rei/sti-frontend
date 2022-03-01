f1b075878920f1dcb6d3beaab230143822d034a0
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-03-01 17:43:26 +0100

    removed unused code

--- app.js
+++ app.js
@@ -19,14 +19,6 @@ app.get("/images", function (req, res){
   res.sendFile(__dirname + "/public/images");
 });
 
-// app.get("/football", function (req, res) {
-//   res.sendFile(__dirname + "/public/index.html");
-// });
-
-// app.get("/hypnosismic", function (req, res) {
-//     res.sendFile(__dirname + "/public/index.html");
-// });
-
 app.listen(PORT, function () {
   console.log(`Server started on port ${PORT}`);
 });
